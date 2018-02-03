aed868dd352e6cd232823a1606936a46ea0cdf48
numnumstyle <35936049+numnumstyle@users.noreply.github.com>
2018-02-03 21:29:54 +0100

    Add files via upload

---
+++ Numer.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int numer;
+	
+	cout << "Wpisz numer telefonu: "; cin >> numer; 
+	
+	cout << numer << endl;
+	
+	system("pause");
+	return 0;
+}
---
+++ Wpisz imie.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string imie;
+	
+	cout << "Wpisz imie: "; cin >> imie; 
+	
+	cout << imie << endl;
+	
+	system("pause");
+	return 0;
+}
